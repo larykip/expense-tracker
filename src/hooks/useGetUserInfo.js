42ad68c48da6fbec6c62cf6fd54f0627a491b2bc
hillary-254 <hillarykim001@gmail.com>
2023-10-12 19:30:48 +0300

    fixed build issue

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,6 +1,6 @@
 const useGetUserInfo = () => {
     //here we deserialize the data from string back into an object
-  const {name,  userID, profilePhoto, isAuth} = JSON.parse(localStorage.getItem("auth"))
+  const {name,  userID, profilePhoto, isAuth} = JSON.parse(localStorage.getItem("auth")) || {}
 
   //we return the individual values of the object
   return {name,  userID, profilePhoto, isAuth}
